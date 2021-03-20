dc654ef0bdadcd09091928ea1234a08e45105c17
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2021-03-20 13:49:17 +0800

    Detach routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import { useState, useEffect } from "react";
-import RouteBox from "./route-box";
-import Header from "./header";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import RouteBox from './route-box';
+import Header from './header';
+import './App.css';
+import { routes2Home, routes2Work } from './routes';
 
 function App() {
-  const routes2Work = [
-    { from: "0182", to: "0126" }, // 捷運古亭站(2號出口)>敦化基隆路口
-    { from: "0087", to: "0126" }, // 捷運大安站>敦化基隆路口
-    { from: "0032", to: "0126" }, // 捷運六張犁站>敦化基隆路口
-    { from: "0306", to: "0126" }, // 捷運古亭站(6號出口)>敦化基隆路口
-  ];
-  const routes2Home = [
-    { from: "0126", to: "0087" }, // 敦化基隆路口>捷運大安站
-    { from: "500101092", to: "500101001" }, // 和平實小東北側>捷運六張犁站
-    { from: "500101092", to: "500101002" }, // 和平實小東北側>復興南路二段273號前
-    { from: "500101091", to: "500101001" }, // 國北教大側門南側>捷運六張犁站
-    { from: "500101091", to: "500101002" }, // 國北教大側門南側>復興南路二段273號前
-    { from: "500101019", to: "500101001" }, // 和平臥龍街口>捷運六張犁站
-    { from: "500101019", to: "500101002" }, // 和平臥龍街口>復興南路二段273號前
-    { from: "0126", to: "0032" }, //敦化基隆路口>捷運六張犁站
-  ];
   const safetyCount = 3;
 
   const [stops, setStops] = useState([]);
   const [target, setTarget] = useState(() => {
-    return new Date().getHours() >= 12 ? "home" : "work";
+    return new Date().getHours() >= 12 ? 'home' : 'work';
   });
   const [selectedView, setSelectedView] = useState([]);
   useEffect(() => {
     const v1Url =
-      "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json";
+      'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json';
     const v2Url =
-      "https://apis.youbike.com.tw/api/front/station/all?lang=tw&type=2";
+      'https://apis.youbike.com.tw/api/front/station/all?lang=tw&type=2';
 
     Promise.all([
       fetch(v1Url).then((res) => res.json()),
@@ -51,7 +36,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setSelectedView(target === "work" ? routes2Work : routes2Home);
+    setSelectedView(target === 'work' ? routes2Work : routes2Home);
   }, [target]);
 
   return (
